ea9ebfce21d8051cbb31b78ffcf7463907a05674
Mark Ruys <mark@paracas.nl>
2019-05-19 23:39:22 +0200

    Switch from maestrodev/wget to puppet/archive

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -6,7 +6,7 @@ describe 'cachetool' do
 
     it { should contain_wget__fetch('cachetool_phar') }
 
-    it { should contain_exec('cachetool_set_permissions').that_requires('Wget::Fetch[cachetool_phar]')  }
+    it { should contain_exec('cachetool_set_permissions').that_requires('Archive[cachetool_phar]')  }
 
     it do
         should contain_file('cachetool_yml').with(
